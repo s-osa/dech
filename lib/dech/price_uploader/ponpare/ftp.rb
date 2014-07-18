0969ae52cc37396cdd619937240583ec7c9632c3
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-07-18 10:42:00 +0900

    Dech::PriceUploader::Ponpare::FTP#ready? #10

--- lib/dech/price_uploader/ponpare/ftp.rb
+++ lib/dech/price_uploader/ponpare/ftp.rb
@@ -1,13 +1,21 @@
+require "net/ftp"
+
 module Dech
   module PriceUploader
     module Ponpare
       class FTP
+        attr_accessor :username, :host, :path
+
         def initialize(args={})
           @products = args[:products] || []
           @username = args[:username]
           @password = args[:password]
           @host     = args[:host] || "ftps.ponparemall.com"
-          @path     = args[:path] || "/"
+          @path     = args[:path] || "/item.csv"
+        end
+
+        def ready?
+          Net::FTP.open(@host, @username, @password){|ftp| ftp.list(path).empty? }
         end
       end
     end
--- spec/dech/price_uploader/ponpare/ftp_spec.rb
+++ spec/dech/price_uploader/ponpare/ftp_spec.rb
@@ -1,18 +1,51 @@
 require "spec_helper"
 
 describe Dech::PriceUploader::Ponpare::FTP do
+  let(:dech) {
+    Dech::PriceUploader::Ponpare::FTP.new(
+      products: {},
+      username: "username",
+      password: "password",
+      host:     "example.com"
+  )
+  }
+
   describe "initialize" do
     context "given no args" do
       it "should create an instance successfully" do
-        dech = Dech::PriceUploader::Ponpare::FTP.new
         expect(dech).to be_an_instance_of(Dech::PriceUploader::Ponpare::FTP)
       end
     end
 
     context "given some args" do
       it "should create an instance successfully" do
-        dech = Dech::PriceUploader::Ponpare::FTP.new(products: {}, uri: "")
         expect(dech).to be_an_instance_of(Dech::PriceUploader::Ponpare::FTP)
+      end
+    end
+  end
+
+  describe "#ready?" do
+    context "some files in the server" do
+      before do
+        ftp = double("ftp")
+        allow(ftp).to receive(:list).with(dech.path).and_return([dech.path])
+        allow(Net::FTP).to receive(:open).and_yield(ftp)
+      end
+
+      it "should be false" do
+        expect(dech.ready?).to be false
+      end
+    end
+
+    context "any files in the server" do
+      before do
+        ftp = double("ftp")
+        allow(ftp).to receive(:list).and_return([])
+        allow(Net::FTP).to receive(:open).and_yield(ftp)
+      end
+
+      it "should be true" do
+        expect(dech.ready?).to be true
       end
     end
   end
